9254e00edb611aebea1e0cd8471de586a81a83ab
Cangkir <115875472+CangkirKosong@users.noreply.github.com>
2023-06-23 10:52:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,17 +5,17 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 LOGGER = logging.getLogger(__name__)
 
 class Config(object):
-    APP_ID = int(os.environ.get("APP_ID", 00000))
-    API_HASH = os.environ.get("API_HASH", "0000")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "00000")
-    OWNER_ID = int(os.environ.get('OWNER_ID', 1257782782)) # give your owner id # if given 0 shell will not works
-    AUTH_IDS = [int(x) for x in os.environ.get("AUTH_IDS", "0").split()] # if open to everyone give 0
+    APP_ID = int(os.environ.get("APP_ID", 18050628))
+    API_HASH = os.environ.get("API_HASH", "89f8ac516563145c27a185d280fdcb01")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6077015920:AAGrwuQyIWIHq_64755SJRinu0c1HWvYIhM")
+    OWNER_ID = int(os.environ.get('OWNER_ID', 5165442029)) # give your owner id # if given 0 shell will not works
+    AUTH_IDS = [int(x) for x in os.environ.get("AUTH_IDS", "-1001877236737").split()] # if open to everyone give 0
     AUTH_IDS.append(OWNER_ID)
-    QOBUZ_MAIL = os.environ.get("QOBUZ_MAIL", "xxxx")
-    QOBUZ_PASS = os.environ.get("QOBUZ_PASS", "xxxx")
+    QOBUZ_MAIL = os.environ.get("QOBUZ_MAIL", "natamalceva80@netek.me")
+    QOBUZ_PASS = os.environ.get("QOBUZ_PASS", "Qobuz2023")
     QOBUZ_QUAL = int(os.environ.get("QOBUZ_QUAL", 27))
     HEROKU_API_KEY = os.environ.get('HEROKU_API_KEY', None)
     HEROKU_APP_NAME = os.environ.get('HEROKU_APP_NAME', None)
     UPDATE_ALL = bool(os.environ.get("UPDATE_ALL", True))
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001821015909")
     botStartTime = time.time() # dont touch
